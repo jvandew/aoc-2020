d6123b6c1171b33a38fcc02acf2be2e28645cd94
Jacob Van De Weert <vandeweertj@gmail.com>
2020-12-21 19:05:55 -0500

    implement day 3 part 1
    https://adventofcode.com/2020/day/3

---
+++ src/bin/day3_1.rs
@@ -0,0 +1,15 @@
+use lib::day3::grid::Grid;
+use lib::runner::file_input_runner::FileInputRunner;
+use std::error::Error;
+
+/* See https://adventofcode.com/2020/day/3 for details. */
+fn main() -> Result<(), Box<dyn Error>> {
+  let runner = FileInputRunner::new("resources/day3/input.txt")?;
+  runner.run(|lines| {
+    let grid = Grid::parse(lines)?;
+    let tree_count = grid.traverse_and_count_trees(3, 1);
+
+    println!("The answer is: {:?}", tree_count);
+    Ok(())
+  })
+}
---
+++ src/lib/day3/grid.rs
@@ -0,0 +1,70 @@
+use std::io::{BufRead, Error as IOError, ErrorKind as IOErrorKind, Lines};
+
+const GRID_WIDTH: usize = 31;
+const GRID_HEIGHT: usize = 323;
+const TREE_CHAR: char = '#';
+const EMPTY_CHAR: char = '.';
+
+#[derive(Debug)]
+enum GridSquare {
+  Empty,
+  Tree,
+}
+
+impl GridSquare {
+  fn from_char(character: char) -> Result<GridSquare, IOError> {
+    match character {
+      EMPTY_CHAR => Ok(GridSquare::Empty),
+      TREE_CHAR => Ok(GridSquare::Tree),
+      other => Err(IOError::new(
+        IOErrorKind::InvalidData,
+        format!("Invalid grid character: '{:?}'", other),
+      )),
+    }
+  }
+}
+
+#[derive(Debug)]
+struct GridRow {
+  squares: Vec<GridSquare>,
+}
+
+impl GridRow {
+  fn parse(line: String) -> Result<GridRow, IOError> {
+    let squares = line
+      .chars()
+      .flat_map(|character| GridSquare::from_char(character))
+      .collect();
+    Ok(GridRow { squares })
+  }
+}
+
+#[derive(Debug)]
+pub struct Grid {
+  rows: Vec<GridRow>,
+}
+
+impl Grid {
+  pub fn parse<L: BufRead>(lines: Lines<L>) -> Result<Grid, IOError> {
+    let rows = lines.flat_map(|line| GridRow::parse(line?)).collect();
+    Ok(Grid { rows })
+  }
+
+  pub fn traverse_and_count_trees(&self, right_step: usize, down_step: usize) -> u32 {
+    let mut row = 0;
+    let mut column = 0;
+    let mut tree_count = 0;
+
+    while row < GRID_HEIGHT - down_step {
+      column = (column + right_step) % GRID_WIDTH;
+      row += down_step;
+
+      match self.rows[row].squares[column] {
+        GridSquare::Empty => (),
+        GridSquare::Tree => tree_count += 1,
+      };
+    }
+
+    tree_count
+  }
+}
